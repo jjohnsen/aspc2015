a95a4b843bc142f71e39dc7acaf99cb3dee54ce4
Jan Åge Johnsen <jaj@skill.no>
2015-03-02 10:04:27 +0100

    Sphero "integration"

---
+++ sphero/README.txt
@@ -0,0 +1,40 @@
+SpheroNET v0.1 by Thomas Bladh
+
+** INFO **
+
+SpheroNET is a Third party developed framework for 
+interacting with the Orbotix Sphero robot from .NET 
+applications.
+
+This software is experimental and incomplete and is 
+likely to crash if you so much as look at it funny.
+
+This software is open source and may be freely 
+distributed and modified.
+
+** ACKNOWLEDGEMENT **
+
+Orbotix for creating the Sphero and providing such 
+excellent documentation on it
+http://www.gosphero.com
+
+The people behind the 32feet bluetooth framework for .NET
+http://32feet.codeplex.com/
+
+** DISCLAIMER **
+
+THIS SOFTWARE IS EXPERIMENTAL AND IS 
+PROVIDED "AS IS". IT IS ASSUMED THAT THE 
+USER HAS EXPERIENCE WITH AND CAN 
+HANDLE THE VARIOUS FORMS OF 
+MALFUNCTION WHICH MAY ARISE AS A 
+RESULT OF THE USE OF UNTESTED 
+EXPERIMENTAL SOFTWARE. IN NO EVENT 
+SHALL THE DEVELOPER BE LIABLE FOR ANY 
+DAMAGES WHATSOEVER (INCLUDING, 
+WITHOUT LIMITATION,LOST PROFITS, 
+BUSINESS INTERRUPTION, OR LOSS OF 
+INFORMATION) ARISING OUT OF THE USE OF 
+OR INABILITY TO USE THE SOFTWARE.
+
+Thomas Bladh, 1 January 2013.
---
+++ sphero/SpheroTest/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Drawing;
+using SpheroNET;
+using RedCorona.Net;
+
+namespace SpheroTest
+{
+    class Program
+    {
+        static Sphero sphero = null;
+
+        private static IEnumerable<string> GetOrbBasicLinesFromFile(string filePath)
+        {
+            var rawLines = File.ReadLines(filePath, Encoding.UTF8);
+            var result = new List<string>();
+            foreach(var line in rawLines)
+            { 
+                 if (!string.IsNullOrEmpty(line) && line[0] != '\'')
+                 {
+                     result.Add(line + "\r");
+                 }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            SpheroConnector spheroConnector = new SpheroConnector();
+
+            spheroConnector.Scan();
+            var deviceNames = spheroConnector.DeviceNames;
+            int id = -1;
+
+            for (int i = 0; i < deviceNames.Count; i++)
+            { 
+                Console.WriteLine("{0}: {1}", i, deviceNames[i]);
+                if (deviceNames[i].Equals("Sphero-BPR"))
+                {
+                    id = i;
+                    Console.WriteLine("FOUND ONE!!!!");  
+                }
+            }
+
+            if (id >= 0)
+            {
+                sphero = spheroConnector.Connect(id);
+
+                Console.WriteLine("CONNECTED");
+            }
+
+            var server = new Server();
+            server.start();
+        
+            Console.ReadKey();
+        }
+
+        public static void Blink()
+        {
+            byte r, g, b;
+            g = 0;
+            b = 0;
+            for (byte i = 0; i < 50; i++)
+            {
+                if (i % 2 == 0) {
+                    r = 255;
+                }
+                else
+                {
+                    r = 0;
+                }
+                sphero.SetRGBLEDOutput(r, g, b);
+                Thread.Sleep(200);
+            }
+        }
+    }
+
+    public class Server
+    {
+        public void start()
+        {
+            var server = new RedCorona.Net.Server(9090);
+            var filereder = new MyHandler();
+
+            HttpServer http = new HttpServer(server);
+            http.Handlers.Add(filereder);
+        }
+    }
+
+    public class MyHandler : SubstitutingFileReader
+    {
+        public override string GetValue(HttpRequest req, string tag)
+        {
+            Console.WriteLine("OMG");
+            return base.GetValue(req, tag);
+        }
+
+        public override bool Process(HttpServer server, HttpRequest request, HttpResponse response)
+        {
+            Program.Blink();
+            return false;
+        }
+    }
+}
